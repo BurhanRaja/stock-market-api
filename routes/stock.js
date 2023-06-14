7b6a826b00145697dafa1a922d9e7986e1ce1006
BurhanuddinRaja <mails4burhan@gmail.com>
2023-06-14 18:54:14 +0530

    completed stock all

--- routes/stock.js
+++ routes/stock.js
@@ -3,7 +3,15 @@ import stock from "../controller/stock.js";
 
 const router = Router();
 
-router.get("/details/:name/:id", stock.getStockDetails);
+router.get("/overview/:name/:id", stock.getStockOverview);
+
+router.get("/indicators/:name/:id", stock.getStocIndicators);
+
+router.get("/performance/:name/:id", stock.getStocPerformance);
+
+router.get("/ratios/:name/:id", stock.getStockRatios);
+
+router.get("/shareholderreturn/:name/:id", stock.getStockShareHoldersReturn);
 
 router.get("/price/:name/:id", stock.getStockPrice);
 
